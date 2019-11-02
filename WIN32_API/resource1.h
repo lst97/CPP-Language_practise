2d68a64bd4955a59c86254958746719a098bfbd2
SIO TOU LAI (Nelson) <laisiotou1997@gmail.com>
2019-11-02 15:26:29 +1100

    Create resource1.h

---
+++ WIN32_API/resource1.h
@@ -0,0 +1,22 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by Win32Demo1.rc
+//
+#define IDD_DIALOG_LOGIN                101
+#define IDC_BUTTON_OK                   1004
+#define IDC_EDIT_NAME                   1005
+#define IDC_EDIT_PASSWORD               1006
+#define IDC_STATIC_NAME                 1007
+#define IDC_BUTTON_CANCEL               1008
+#define IDC_STATIC_PASSWORD             -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        103
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1005
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
