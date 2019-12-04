dbed1cb8c04f93c094f92007a57a99a94677297e
SIO TOU LAI (Nelson) <laisiotou1997@gmail.com>
2019-12-04 18:08:43 +1100

    Create resource.h

---
+++ WIN32_API/Threads/Mutex/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by cppTutorial.rc
+//
+#define IDD_DIALOG_TIMER                101
+#define IDC_EDIT_TIMER                  1001
+#define IDC_BUTTON_START                1002
+#define IDC_BUTTON_RESUME               1003
+#define IDC_EDIT_CUSTOMER1              1003
+#define IDC_BUTTON_PAUSE                1004
+#define IDC_BUTTON_STOP                 1005
+#define IDC_EDIT_CUSTOMER2              1006
+#define IDC_EDIT_TIMER4                 1007
+#define IDC_EDIT_CUSTOMER3              1007
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
