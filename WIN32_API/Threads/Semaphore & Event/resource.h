d63f8267b8e027b3a7ac41267fc6964ea5fb1f93
SIO TOU LAI (Nelson) <laisiotou1997@gmail.com>
2019-12-07 18:19:47 +1100

    Create resource.h

---
+++ WIN32_API/Threads/Semaphore & Event/resource.h
@@ -0,0 +1,25 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by SemaphoreProject.rc
+//
+#define IDD_DIALOG_GRABFOOD             101
+#define IDC_BUTTON_START                1001
+#define IDC_EDIT_ORDERS                 1002
+#define IDC_EDIT_WAITING1               1003
+#define IDC_EDIT_WAITING2               1004
+#define IDC_EDIT_CUSTOMER1              1005
+#define IDC_EDIT_CUSTOMER2              1006
+#define IDC_EDIT_CUSTOMER3              1007
+#define IDC_EDIT_CUSTOMER4              1008
+#define IDC_BUTTON_CLOSE                1009
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
